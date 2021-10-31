e1d9dd8743de052c9dd218bde854975489a6314e
cherif, Ouedraogo <cherif.ouedraogo@outlook.com>
2021-10-30 20:13:26 -0500

    Replacing corrupted Initial commit

---
+++ creation-wand-api/src/main/java/com/burkindi/creationwandapi/creationwandapi/CreationWandApiApplication.java
@@ -0,0 +1,13 @@
+package com.burkindi.creationwandapi.creationwandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationWandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationWandApiApplication.class, args);
+	}
+
+}
---
+++ creation-wand-api/src/test/java/com/burkindi/creationwandapi/creationwandapi/CreationWandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burkindi.creationwandapi.creationwandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationWandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
